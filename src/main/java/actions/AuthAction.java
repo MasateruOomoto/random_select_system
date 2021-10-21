9fb5f5019abf1e40f971e52932059e3d857ed4ba
masateru <masateru@LAPTOP-B3H9AUCL>
2021-10-22 01:48:23 +0900

    ログインアクションの作成

--- src/main/java/actions/AuthAction.java
+++ src/main/java/actions/AuthAction.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 
+import actions.views.UserView;
 import constants.AttributeConst;
 import constants.ForwardConst;
+import constants.MessageConst;
+import constants.PropertyConst;
 import services.UserService;
 
 /**
@@ -51,6 +54,50 @@ public class AuthAction extends ActionBase {
 
         //ログイン画面を表示
         forward(ForwardConst.FW_LOGIN);
+    }
+
+    /**
+     * ログイン処理を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void login() throws ServletException, IOException {
+
+        String userId = getRequestParam(AttributeConst.USER_USER_ID);
+        String plainPass = getRequestParam(AttributeConst.USER_PASS);
+        String pepper = getContextScope(PropertyConst.PEPPER);
+
+        //有効なユーザーか認証する
+        Boolean isValidUser = service.validateLogin(userId, plainPass, pepper);
+
+        if (isValidUser) {
+            //認証成功の場合
+
+            //CSRF対策 tokenのチェック
+            if (checkToken()) {
+
+                //ログインしたユーザーのDBデータを取得
+                UserView uv = service.findOne(userId, plainPass, pepper);
+                //セッションにログインしたユーザーを設定
+                putSessionScope(AttributeConst.LOGIN_USER, uv);
+                //セッションにログイン完了のフラッシュメッセージを設定
+                putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGINED.getMessage());
+                //トップページへリダイレクト
+                redirect(ForwardConst.ACT_TOP, ForwardConst.CMD_INDEX);
+            }
+        } else {
+            //認証失敗の場合
+
+            //CSRF対策用トークンを設定
+            putRequestScope(AttributeConst.TOKEN, getTokenId());
+            //認証失敗エラーメッセージ表示フラグをたてる
+            putRequestScope(AttributeConst.LOGIN_ERR, true);
+            //入力された従業員コードを設定
+            putRequestScope(AttributeConst.USER_USER_ID, userId);
+
+            //ログイン画面を表示
+            forward(ForwardConst.FW_LOGIN);
+        }
     }
 
 }
